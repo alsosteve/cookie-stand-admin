32a140e759a4d8b54c0569965252450bc37483fd
Steve Ngo <stevehngo@hotmail.com>
2022-06-09 06:20:03 -0400

    lab27 done

--- pages/components/form.jsx
+++ pages/components/form.jsx
@@ -1,36 +1,42 @@
 import { useState } from 'react';
 
-export default function Form({ submitHandler }) {
+export default function Form({ handleSubmit, handleChange }) {
   
   return (
-    <form onSubmit={submitHandler} className='bg-[#6FE6B7] py-3 pl-3 rounded-md w-5/6'>
-      
-      <div className="w-full">
-        <label>Location</label>
-        <input className="w-full"/>
-      </div>
+    <form onSubmit={handleSubmit} className='bg-[#6FE6B7] space-y-3 p-3 rounded-md w-5/6'>
+    <h2 className='font-semibold'> Create Cookie Stand</h2>
+    <div className="w-full">
+      <label>
+        <span>Location</span>
+        <input className="w-full" onChange={handleChange} name="location" type="text" placeholder="Paris"/>
+      </label>
+    </div>
 
-      <div className="flex flex-nowrap">
-      <div className="flex flex-col">
-        <label >Minimum Customer per Hour</label>
-        <input/>
+    <div className="flex flex-nowrap">
+    <div className="flex flex-col">
+      <label>
+        <span >Minimum Customer per Hour</span>
+         <input onChange={handleChange} name="min" type="text" placeholder="2"/>
+        </label>
+    </div>
+    <div className="flex flex-col">
+      <label>
+        <span >Maximum Customer per Hour</span>
+         <input onChange={handleChange} name="max" type="text" placeholder="7"/>
+      </label>
+    </div>
+    <div className="flex flex-col">
+      <label>
+        <span >Average Cookies per Sale</span>
+         <input onChange={handleChange} name="ave" type="text" placeholder="4.5"/>
+      </label>
+    </div>
+    <div className="h-100 w-full md:w-1/4 ">
+        <button className="h-full block w-full bg-emerald-500 text-sm text-gray-700  font-bold " type="submit">
+          Create
+        </button>
       </div>
-      <div className="flex flex-col">
-        <label >Maximum Customer per Hour</label>
-        <input/>
-      </div>
-      <div className="flex flex-col">
-        <label >Average Cookies per Sale</label>
-        <input/>
-      </div>
-      <div className="h-100 w-full md:w-1/4 ">
-          <button type="submit" className="h-full block w-full bg-emerald-500 text-gray-700  rounded-md font-bold ">
-            Create
-          </button>
-        </div>
-      </div>
-      
-    </form>
+    </div>
+  </form>
   )
-
 }
--- pages/components/main.jsx
+++ pages/components/main.jsx
@@ -6,6 +6,7 @@ export default function Main() {
   let [min, setMin] = useState("2");
   let [max, setMax] = useState("4");
   let [ave, setAve] = useState("2.5")
+  
 
   function changeData(data) {
     setLocation(data.location.value);
@@ -14,18 +15,22 @@ export default function Main() {
     setAve(data.ave.value);
   }
 
-  function submitHandler(event) {
-    console.log(event)
-    let { value } = event.target
-    changeData(value)
-  }
+
+  const handleSubmit = (event) => {
+    if (event) event.preventDefault();
+    changeData(event.target);
+  };
+
+  const handleChange = (event) => {
+    let { value } = event.target;
+  };
 
   return (
     <div className="py-6">
-      <Form submitHandler={submitHandler}/>
-
-      <p className="py-6 text-xs">Report Table Coming Soon...</p>
-      <p className="text-xs">"location":"{location}","minCustomers":"{min}","maxCustomers":"{max}","aveCookies":"{ave}"</p>
+      <Form handleSubmit={handleSubmit} handleChange={handleChange}/>
+     
+      <p>Report Table Coming Soon...</p>
+      <p>"location":"{location}","minCustomers":"{min}","maxCustomers":"{max}","aveCookies":"{ave}"</p>
     </div>
   )
 }
